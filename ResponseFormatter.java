f77964355a5d5ed95e1d5f5806757cacf9716c58
dervvilang <147318490+dervvilang@users.noreply.github.com>
2024-12-09 15:30:18 +0300

    Update ResponseFormatter.java

--- ResponseFormatter.java
+++ ResponseFormatter.java
@@ -5,25 +5,17 @@ import java.util.List;
 
 public class ResponseFormatter {
 
-    private final Gson gson; // Для парсинга JSON
+    private final Gson gson;
 
     public ResponseFormatter() {
         this.gson = new Gson();
     }
 
-    /**
-     * Форматирует пользователей для отображения в графическом интерфейсе.
-     *
-     * @param jsonResponse JSON-строка, содержащая данные пользователей.
-     * @return Отформатированная строка для вывода.
-     */
     public String formatUsersForDisplay(String jsonResponse) {
         try {
-            // Определяем тип данных
             Type userListType = new TypeToken<List<User>>() {}.getType();
             List<User> users = gson.fromJson(jsonResponse, userListType);
 
-            // Форматируем пользователей
             StringBuilder builder = new StringBuilder();
             int userNumber = 1;
             for (User user : users) {
@@ -39,19 +31,11 @@ public class ResponseFormatter {
         }
     }
 
-    /**
-     * Форматирует задачи для отображения в графическом интерфейсе.
-     *
-     * @param jsonResponse JSON-строка, содержащая данные задач.
-     * @return Отформатированная строка для вывода.
-     */
     public String formatTodosForDisplay(String jsonResponse) {
         try {
-            // Определяем тип данных
             Type todoListType = new TypeToken<List<Todo>>() {}.getType();
             List<Todo> todos = gson.fromJson(jsonResponse, todoListType);
 
-            // Форматируем задачи
             StringBuilder builder = new StringBuilder();
             int todoNumber = 1;
             for (Todo todo : todos) {
@@ -66,20 +50,10 @@ public class ResponseFormatter {
         }
     }
 
-    /**
-     * Устаревший метод для консольного вывода пользователей.
-     *
-     * @param jsonResponse JSON-строка, содержащая данные пользователей.
-     */
     public void printUsers(String jsonResponse) {
         System.out.println(formatUsersForDisplay(jsonResponse));
     }
 
-    /**
-     * Устаревший метод для консольного вывода задач.
-     *
-     * @param jsonResponse JSON-строка, содержащая данные задач.
-     */
     public void printTodos(String jsonResponse) {
         System.out.println(formatTodosForDisplay(jsonResponse));
     }
